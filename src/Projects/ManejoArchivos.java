e6742e3fabe752a52a87d1d720660b5a058a0162
Miguel-unl <miguel.armas@unl.edu.ec>
2024-12-11 21:26:21 -0500

    Organizacion del código, correcciones y separacion de ćodigos

---
+++ src/Projects/ManejoArchivos.java
@@ -0,0 +1,24 @@
+package Projects;
+
+public class ManejoArchivos {
+    public static String devolverLinea() {
+        /*
+         * UNA MANERA A CONSIDERAR PARA RECUPERAR LAS LINEAS DE LOS ARCHIVOS
+         * // Construir la ruta
+         * Path direccion = Paths.get(rutaDeArchivo);
+         * // Transformar path a archivo txt
+         * File archivo = direccion.toFile();
+         *
+         * // Condicion para verificar si el archivo existe
+         * if (archivo.exists()) {
+         * // Bloque para leer un archivo evitando la posibilidad de errores
+         * try (Scanner lectorArchivoTXT = new Scanner(archivo)) {
+         * // Bucle while para verificar que se lean todas las lineas del archivo txt
+         * while (lectorArchivoTXT.hasNextLine()) {
+         * // Almacenar la linea leida en un String
+         * String linea = lectorArchivoTXT.nextLine();
+         * //Linea vacia que sera el espacio para mejorar el orden de las preguntas
+         */
+        return "";
+    }
+}
